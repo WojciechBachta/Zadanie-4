e40c1e4bfd8e2493fa94731a7c24f79921d35860
WojciechBachta <62569872+WojciechBachta@users.noreply.github.com>
2020-05-05 16:26:58 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
-#include "Wektor.hh"
-#include "Macierz.hh"
-#include "UkladRownanLiniowych.hh"
 
-
+//#include "Macierz.hh"
+#include "SUkladRownanLiniowych.hh"
+#include "SWektor.hh"
+#include "rozmiar.h"
+#include "LZespolona.hh"
+#include "SMacierz.hh"
 
 
 
@@ -23,9 +25,37 @@ using namespace std;
 
 int main()
 {
-  UkladRownanLiniowych   UklRown;   // To tylko przykladowe definicje zmiennej
+  SUkladRownanLiniowych<double,ROZMIAR>   UklRown;   // To tylko przykladowe definicje zmiennej
 
 
+/*
+    SMacierz<LZespolona, ROZMIAR> M;
+    SWektor<LZespolona, ROZMIAR> W;
+    SWektor<LZespolona, ROZMIAR> H;
+    SWektor<LZespolona, ROZMIAR> G;
+    LZespolona Z={2,5};
+    LZespolona X={4,1};
+    LZespolona C={1,5};
+    LZespolona V={3,1};
+    LZespolona B={2,8};
+    LZespolona N={5,9};
+
+
+    W.Zamien(0,Z);
+    W.Zamien(1,X);
+
+    H.Zamien(0,C);
+    H.Zamien(1,V);
+
+    G.Zamien(0,B);
+    G.Zamien(1,N);
+
+    M.ZamienWiersz(0, W);
+    M.ZamienWiersz(1, H);
+
+    UklRown.ZamienMacierz(M);
+    UklRown.ZamienWektor(G);
+*/
   cout << endl << " Start programu " << endl << endl;
 
 std::fstream plik;
@@ -33,17 +63,25 @@ plik.open( "macierz.txt", std::ios::in );
 
 plik >> UklRown;
 
-Wektor Wyniki=UklRown.oblicz();
+SWektor<double,ROZMIAR> Wyniki=UklRown.oblicz();
 
   cout << UklRown << "\n\n";
+    //cout << "Macierz odwrotna:" <<endl;
+      //cout << UklRown.WezMacierz().MacierzOdwrotna() <<endl;
   cout << "     Rozwiazanie: " << Wyniki << "\n";
   cout << endl;
   cout << "     x1 = "<< Wyniki.Wez(0) <<endl;
   cout << "     x2 = "<< Wyniki.Wez(1) <<endl;
   cout << "     x3 = "<< Wyniki.Wez(2) <<endl;
+  cout << "     x4 = "<< Wyniki.Wez(3) <<endl;
+  cout << "     x5 = "<< Wyniki.Wez(4) <<endl;
+  cout << "     x6 = "<< Wyniki.Wez(5) <<endl;
   cout << endl;
   cout << "     Wektor bledu:  Ax - b  = " << UklRown.blad() << "\n";
-  cout << "     Dlugosc wektora bledu: |Ax - b| = " << UklRown.blad().dlugosc() << "\n";
+  //cout << "     Dlugosc wektora bledu: |Ax - b| = " << UklRown.blad().dlugosc() << "\n";
   cout << "     Wyznacznik = " << UklRown.WezMacierz().Wyznacznik() << "\n";
   cout << endl;
+
+
 }
+
